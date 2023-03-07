cdd9b70e1bb44fb33b682043457d3917b567a89c
Z <z>
2023-03-07 11:37:49 -0400

    build: add axios and react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}></QueryClientProvider>
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <App />
     </MantineProvider>
