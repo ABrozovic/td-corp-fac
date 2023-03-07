bd01319b00e8c40fb5354c97f43cb908c4b120e6
Z <z>
2023-03-07 11:35:31 -0400

    feat: add helpers to assert reponse types

---
+++ src/utils/type-helpers.ts
@@ -0,0 +1,30 @@
+import { Result } from '../types/api';
+import { Directorio } from '../types/directorio';
+import { Producto } from '../types/producto';
+import { ImpuestosResponse } from '../types/responses';
+function isnt(val: unknown, typeName: string) {
+  return new Error(`\`${val}\` no es ${typeName}`);
+}
+
+export const isDirectorio = (val: Result): asserts val is Directorio => {
+  if (!('dirId' in val)) throw isnt(val, 'un directorio');
+};
+export const isImpuestosResponse = (val: Result): asserts val is ImpuestosResponse => {
+  if (!('Qr' in val)) throw isnt(val, 'una respuesta de factura');
+};
+
+export const isProductoArray = (val: Result[] | undefined): asserts val is Producto[] => {
+  if (Array.isArray(val)) {
+    if (!val.every((item) => 'artId' in item && 'artNombre' in item))
+      throw isnt(val, 'un array de productos');
+    return;
+  }
+};
+export const isDirectorioArray = (
+  val: Result[] | undefined,
+): asserts val is Directorio[] => {
+  if (Array.isArray(val)) {
+    if (!val.every((item) => 'dirId' in item && 'dirNombre' in item))
+      throw isnt(val, 'un array de usuarios');
+  }
+};
