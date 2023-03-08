243279107822eebb04517453fc5a723592ebb2d8
Z <z>
2023-03-08 11:26:35 -0400

    feat: switch to themeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,30 @@
-import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
+import { ThemeProvider } from './components/theme-provider';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      staleTime: 10 * 1000,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}></QueryClientProvider>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
+
   document.getElementById('root'),
 );
